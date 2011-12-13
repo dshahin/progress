04e42ede27f2e4cf01b694aa0572b29188c49a24
Dan Shahin <dan@danshahin.com>
2011-12-13 00:14:57 -0800

    Added local storage for memory of progress

--- js/script.js
+++ js/script.js
@@ -4,9 +4,43 @@
 
 
 $(function(){
+	var prog = 0;
+	if (typeof(localStorage) == 'undefined' ) {
+		alert('Your browser does not support HTML5 localStorage. Try upgrading.');
+	}else{
+		prog = localStorage.getItem("progress")
+		//alert('Your browser does  support HTML5 localStorage. :' + prog);
+	}
+
+
+	var prog = parseInt(localStorage.getItem("progress"));
+	console.log(prog);
 	$('#about').dialog({ autoOpen: false ,modal:true, title:'About'});
+	$('#attaboy').dialog({ autoOpen: false ,modal:true, title:'Alert'});
+
+	$( "#progressbar" ).progressbar({value: prog});
 	$('#hey').button().click(function(){
-			$('#about').dialog('open');
-		});
+		$("#about").dialog("open");
+	});
+	$('#step').button().click(function(){
+			var val = localStorage.getItem("progress");
+			val = parseInt(val) + 1;
+			if(val>=100){
+				$("#attaboy").dialog("open").html('great job!');
+				val = 0;
+			}else if (val == 50){
+				$("#attaboy").dialog("open").html('half way there!' );
+			}else if (val == 1){
+				$("#attaboy").dialog("open").html('the journey of 1000 miles begins with the first step');
+			}else{
+				//$("#attaboy").dialog("open").html('you are ' + val + '% of the way there, keep going!' );
+			}
+			localStorage.setItem("progress", val);
+			console.log('click' + localStorage.getItem("progress"));
+			$( "#progressbar" ).progressbar({
+				value: val
+			});
+			
+	});
 		
 });
