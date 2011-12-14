fcf486b03bc74786991c766f4e20561c2d5e7b3a
Dan Shahin <dan@danshahin.com>
2011-12-13 23:38:18 -0800

    added badge info

--- js/script.js
+++ js/script.js
@@ -4,14 +4,22 @@
 
 
 $(function(){
+	
+	function onYouTubePlayerReady(playerid) {
+		 ytplayer = document.getElementById('myytplayer');
+		alert('youtube ready:' +playerid);
+	}
+	
 	console.log('first' + localStorage.getItem("progress"));
 	if(localStorage.getItem("progress") == null){
 		localStorage.setItem("progress", "0");
 	}
 	var prog = localStorage.getItem("progress");
+	chrome.browserAction.setBadgeText({text:prog+'%'});
 	$('#about').dialog({ autoOpen: false ,modal:true, title:'About'});
-	$('#attaboy').dialog({ autoOpen: false ,modal:true, title:'Alert'});
-
+	//$.fx.speeds._default = 500;
+	$('#attaboy').dialog({ autoOpen: false ,modal:true, title:'Success!', height:500, width:650, show: 'puff', hide:'explode'});
+	$('#content').resizable();
 	$( "#progressbar" ).progressbar({value: parseInt(prog)});
 	$('#hey').button().click(function(){ 
 		$("#about").dialog("open").html('you are ' +  localStorage.getItem("progress") + '% of the way there, keep going!' );
@@ -21,14 +29,20 @@ $(function(){
 			if(isNaN(val)){
 				val = 0;
 			}
-			val = parseInt(val) + 1;
+			val = parseInt(val) + 10;
+			chrome.browserAction.setBadgeText({text:val+'%'});
 			if(val>=100){
-				$("#attaboy").dialog("open").html('great job!');
+				$("#content").attr({'height': 550, 'width' : 700});
+				$("#attaboy").dialog("open");
+				$("#movie").show();
 				val = 0;
+				chrome.browserAction.setBadgeText({text:0+'%'});
+
 			}else if (val == 50){
-				$("#attaboy").dialog("open").html('half way there!' );
+				//$("#attaboy").dialog("open").html('half way there!' );
 			}else if (val == 1){
-				$("#attaboy").dialog("open").html('the journey of 1000 miles begins with the first step');
+				//$("#attaboy").dialog("open").html('the journey of 1000 miles begins with the first step');
+				$("#movie").hide();
 			}else{
 				//$("#attaboy").dialog("open").html('you are ' + val + '% of the way there, keep going!' );
 			}
